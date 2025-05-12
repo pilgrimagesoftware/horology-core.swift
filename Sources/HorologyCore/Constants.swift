b6565cc440efd7a249f6ed46e102eae9dea07003
Paul Schifferer <paul@schifferers.net>
2025-05-12 16:15:25 -0700

    Add DateTimeMode

---
+++ Sources/HorologyCore/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Horology
+//
+//  Created by Paul Schifferer on 23/8/17.
+//  Copyright © 2017 Pilgrimage Software. All rights reserved.
+//
+
+
+enum DateTimeMode : String {
+    case dateOnly
+    case timeOnly
+    case dateAndTime
+}
