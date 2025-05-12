bf0531ff35776aad0df715b62873db820122a6a3
Paul Schifferer <paul@schifferers.net>
2025-05-12 16:20:51 -0700

    Tests

---
+++ Tests/HorologyCoreTests/CalendarExtensionTests.swift
@@ -0,0 +1,28 @@
+//
+//  CalendarExtensions.swift
+//  Horology
+//
+//  Created by Paul Schifferer on 29/8/17.
+//  Copyright © 2017 Pilgrimage Software. All rights reserved.
+//
+
+import XCTest
+@testable import HorologyCore
+
+
+final class CalendarExtensionTests: XCTestCase {
+    func testFrom() throws {
+        let calendar = Calendar.from(identifier: "gregorian")
+        XCTAssertEqual(calendar.identifier, .gregorian)
+    }
+
+    func testAsString() throws {
+        let calendar = Calendar.from(identifier: "gregorian")
+        XCTAssertEqual(calendar.asString(), "gregorian")
+    }
+
+    func testLabel() throws {
+        let calendar = Calendar.from(identifier: "gregorian")
+        XCTAssertEqual(calendar.label, "Gregorian")
+    }
+}
