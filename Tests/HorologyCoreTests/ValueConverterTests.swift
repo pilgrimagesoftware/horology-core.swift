8374a97a92ad9ee297ff90c834f562e4033e41dd
Paul Schifferer <paul@schifferers.net>
2025-05-12 17:20:35 -0700

    Add more ValueConverter tests

--- Tests/HorologyCoreTests/ValueConverterTests.swift
+++ Tests/HorologyCoreTests/ValueConverterTests.swift
@@ -9,7 +9,15 @@ import XCTest
 
 
 final class ValueConverterTests: XCTestCase {
-    func testConvertToYears() {
+    func convertToYears() {
+    let result = ValueConverter(value: 13, valueType: .months)
+            .convert(to: .years)
+
+        XCTAssertEqual(result.value, 2)
+        XCTAssertEqual(result.approximate, true)
+    }
+
+    func testConvertToMonths() {
         let result = ValueConverter(value: 5, valueType: .years)
             .convert(to: .months)
 
@@ -17,11 +25,52 @@ final class ValueConverterTests: XCTestCase {
         XCTAssertEqual(result.approximate, false)
     }
 
-    func testConvertToMonths() {
+    func testConvertToDays() {
         let result = ValueConverter(value: 2, valueType: .months)
             .convert(to: .days)
 
-        XCTAssertEqual(result.value, 60)
+        XCTAssertEqual(result.value, 62)
         XCTAssertEqual(result.approximate, true)
     }
+
+    func convertToHours() {
+        let result = ValueConverter(value: 3, valueType: .days)
+            .convert(to: .hours)
+
+        XCTAssertEqual(result.value, 72)
+        XCTAssertEqual(result.approximate, false)
+    }
+
+    func convertToMinutes() {
+        let result = ValueConverter(value: 7, valueType: .hours)
+            .convert(to: .minutes)
+
+        XCTAssertEqual(result.value, 420)
+        XCTAssertEqual(result.approximate, false)
+    }
+
+    func convertToSeconds() {
+        let result = ValueConverter(value: 3, valueType: .hours)
+            .convert(to: .seconds)
+
+        XCTAssertEqual(result.value, 10800)
+        XCTAssertEqual(result.approximate, false)
+    }
+
+    func convertToDogYears() {
+        let result = ValueConverter(value: 3, valueType: .years)
+            .convert(to: .dogYears)
+
+        XCTAssertEqual(result.value, 21)
+        XCTAssertEqual(result.approximate, true)
+    }
+
+    func convertToCatYears() {
+        let result = ValueConverter(value: 3, valueType: .years)
+            .convert(to: .catYears)
+
+        XCTAssertEqual(result.value, 24)
+        XCTAssertEqual(result.approximate, true)
+    }
+
 }
