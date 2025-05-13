bd45978115a7d3125fa8899854936a39ad6e946a
Paul Schifferer <paul@schifferers.net>
2025-05-12 18:33:16 -0700

    Add tests for date converter extension

--- Tests/HorologyCoreTests/DateExtensionsTests.swift
+++ Tests/HorologyCoreTests/DateExtensionsTests.swift
@@ -9,4 +9,23 @@ import XCTest
 
 
 final class DateExtensionsTests: XCTestCase {
+
+    func testConvert() {
+        let date = Date()
+        let fromCalendar = Calendar(identifier: .gregorian)
+        let toCalendar = Calendar(identifier: .buddhist)
+
+        let convertedDate = date.convert(fromCalendar: fromCalendar, toCalendar: toCalendar)
+
+        XCTAssertNotNil(convertedDate)
+    }
+
+    func testConvertWithSameCalendar() {
+        let date = Date()
+        let calendar = Calendar(identifier: .gregorian)
+
+        let convertedDate = date.convert(fromCalendar: calendar, toCalendar: calendar)
+
+        XCTAssertEqual(date, convertedDate)
+    }
 }
