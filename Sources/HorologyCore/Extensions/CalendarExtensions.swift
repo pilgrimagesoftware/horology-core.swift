d64951871c43bdd62936266084d5226b84668b88
Paul Schifferer <paul@schifferers.net>
2025-05-18 06:58:22 -0700

    Use calendar ID map for from function

--- Sources/HorologyCore/Extensions/CalendarExtensions.swift
+++ Sources/HorologyCore/Extensions/CalendarExtensions.swift
@@ -6,76 +6,30 @@
 
 import Foundation
 
+extension Calendar {
 
-public extension Calendar {
-
-    static var identifiers : [Calendar.Identifier] {
+    public static var identifiers: [Calendar.Identifier] {
         return [
-            .buddhist, .chinese, .coptic, .ethiopicAmeteAlem, .ethiopicAmeteMihret, .gregorian, .hebrew, .indian,
-            .islamic, .islamicCivil, .islamicTabular, .islamicUmmAlQura, .iso8601, .japanese, .persian, .republicOfChina
+            .buddhist, .chinese, .coptic, .ethiopicAmeteAlem, .ethiopicAmeteMihret, .gregorian,
+            .hebrew, .indian,
+            .islamic, .islamicCivil, .islamicTabular, .islamicUmmAlQura, .iso8601, .japanese,
+            .persian, .republicOfChina,
         ]
     }
 
-    static func from(identifier : String) throws -> Calendar {
-        switch identifier {
-        case "buddhist":
-            return Calendar(identifier: .buddhist)
-
-        case "chinese":
-            return Calendar(identifier: .chinese)
-
-        case "coptic":
-            return Calendar(identifier: .coptic)
-
-        case "ethiopicAmeteAlem":
-            return Calendar(identifier: .ethiopicAmeteAlem)
-
-        case "ethiopicAmeteMihret":
-            return Calendar(identifier: .ethiopicAmeteMihret)
-
-        case "gregorian":
-            return Calendar(identifier: .gregorian)
-
-        case "hebrew":
-            return Calendar(identifier: .hebrew)
-
-        case "indian":
-            return Calendar(identifier: .indian)
-
-        case "islamic":
-            return Calendar(identifier: .islamic)
-
-        case "islamicCivil":
-            return Calendar(identifier: .islamicCivil)
-
-        case "islamicTabular":
-            return Calendar(identifier: .islamicTabular)
-
-        case "islamicUmmAlQura":
-            return Calendar(identifier: .islamicUmmAlQura)
-
-        case "iso8601":
-            return Calendar(identifier: .iso8601)
-
-        case "japanese":
-            return Calendar(identifier: .japanese)
-
-        case "persian":
-            return Calendar(identifier: .persian)
-
-        case "republicOfChina":
-            return Calendar(identifier: .republicOfChina)
-
-        default:
+    public static func from(identifier: String) throws -> Calendar {
+        guard let id = calendarIds[identifier] else {
             throw HorologyError.invalidCalendarIdentifier(identifier)
         }
+
+        return Calendar(identifier: id)
     }
 
-    func asString() -> String {
+    public func asString() -> String {
         return "\(identifier)"
     }
 
-    var label : String {
+    public var label: String {
         return NSLocalizedString("calendar.identifier.\(self.identifier)", comment: "")
     }
 
