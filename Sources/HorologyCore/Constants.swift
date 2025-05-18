8872c93ccdf6f7bc5459fd7c67a493e7dfa1e69e
Paul Schifferer <paul@schifferers.net>
2025-05-18 06:57:44 -0700

    Add calendar ID constants

--- Sources/HorologyCore/Constants.swift
+++ Sources/HorologyCore/Constants.swift
@@ -4,13 +4,33 @@
 //  Copyright © 2025 Pilgrimage Software. All rights reserved.
 //
 
+import Foundation
 
-public enum DateTimeMode : String {
+public enum DateTimeMode: String {
     case dateOnly
     case timeOnly
     case dateAndTime
 }
 
-public enum HorologyError : Error {
+public enum HorologyError: Error {
     case invalidCalendarIdentifier(String)
 }
+
+public let calendarIds: [String: Calendar.Identifier] = [
+    "buddhist": .buddhist,
+    "chinese": .chinese,
+    "coptic": .coptic,
+    "ethiopicAmeteAlem": .ethiopicAmeteAlem,
+    "ethiopicAmeteMihret": .ethiopicAmeteMihret,
+    "gregorian": .gregorian,
+    "hebrew": .hebrew,
+    "indian": .indian,
+    "islamic": .islamic,
+    "islamicCivil": .islamicCivil,
+    "islamicTabular": .islamicTabular,
+    "islamicUmmAlQura": .islamicUmmAlQura,
+    "iso8601": .iso8601,
+    "japanese": .japanese,
+    "persian": .persian,
+    "republicOfChina": .republicOfChina,
+]
